6be4de4b692f642c4e09f563d6c7a0c94bcd08e3
손상욱 <thstkddnr20@naver.com>
2026-07-24 14:14:57 +0900

    feat: Member email unique 조건 추가

--- src/main/java/github/sangwook/ecommerce/member/domain/Member.java
+++ src/main/java/github/sangwook/ecommerce/member/domain/Member.java
@@ -12,7 +12,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
 
     @Column(name = "password_hash")
@@ -30,3 +30,4 @@ public class Member {
         this.name = name;
     }
 }
+
