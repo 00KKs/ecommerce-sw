ea69cc7056455d47b14f0f7af38c23dc3ea6dd86
thstkddnr20 <thstkddnr20@gmail.com>
2026-07-26 13:11:41 +0900

    feat: 배송지 도메인 설계

---
+++ src/main/java/github/sangwook/ecommerce/member/domain/Address.java
@@ -0,0 +1,49 @@
+package github.sangwook.ecommerce.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "address")
+@Getter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "recipient_name")
+    private String recipientName;
+
+    @Column(name = "recipient_phone")
+    private String recipientPhone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "delivery_request")
+    private String deliveryRequest;
+
+    @Column(name = "is_default")
+    private Boolean isDefault;
+
+    protected Address() {
+    }
+
+    public Address(Long memberId, String deliveryRequest, String recipientPhone, String recipientName, String address, boolean isDefault) {
+        this.memberId = memberId;
+        this.deliveryRequest = deliveryRequest;
+        this.recipientPhone = recipientPhone;
+        this.recipientName = recipientName;
+        this.address = address;
+        this.isDefault = isDefault;
+    }
+}
