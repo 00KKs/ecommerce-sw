2ce4a7768c051b43ce8d100e55c5c66f169637c0
thstkddnr20 <thstkddnr20@gmail.com>
2026-07-30 23:28:57 +0900

    feat: 상품 도메인 작성

---
+++ src/main/java/github/sangwook/ecommerce/catalog/domain/Product.java
@@ -0,0 +1,44 @@
+package github.sangwook.ecommerce.catalog.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sale_status", nullable = false)
+    private ProductSaleStatus saleStatus;
+
+    protected Product() {
+    }
+
+    public Product(Long categoryId, String name, String description) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+        this.saleStatus = ProductSaleStatus.DRAFT;
+    }
+}
