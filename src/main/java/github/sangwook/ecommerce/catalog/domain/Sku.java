5b78573988182162de2edf413da123b4fd1e46ff
손상욱 <thstkddnr20@naver.com>
2026-08-03 19:31:52 +0900

    feat: sku 도메인 생성

---
+++ src/main/java/github/sangwook/ecommerce/catalog/domain/Sku.java
@@ -0,0 +1,38 @@
+package github.sangwook.ecommerce.catalog.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "sku")
+@Getter
+public class Sku {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "option_name")
+    private String optionName;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private SkuSaleStatus status;
+
+    protected Sku() {
+    }
+
+    public Sku(Long productId, String optionName, int price) {
+        this.productId = productId;
+        this.optionName = optionName;
+        this.price = price;
+        this.status = SkuSaleStatus.STOPPED;
+    }
+
+}
