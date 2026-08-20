d01b6f35ca4c6747b4e22c3a195c7c4865adcd92
손상욱 <thstkddnr20@naver.com>
2026-08-20 16:10:21 +0900

    fix: addressAdapter public -> package-private 접근제어로 변경

--- src/main/java/github/sangwook/ecommerce/member/adapter/AddressAdapter.java
+++ src/main/java/github/sangwook/ecommerce/member/adapter/AddressAdapter.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class AddressAdapter implements AddressPort {
+class AddressAdapter implements AddressPort {
 
     private final AddressService addressService;
 
